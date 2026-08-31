e7cc1934200734309c3ba813b7814eb7fba1309b
b1ankk <b1ankk-dev@pm.me>
2026-08-31 19:39:37 +0200

    Add: minimum-knight-moves - submission-10

---
+++ Data Structures & Algorithms/minimum-knight-moves/submission-10.js
@@ -0,0 +1,66 @@
+
+const MOVES = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]]
+
+const key = (a, b) => {
+    return `${a}_${b}`;
+}
+
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {number}
+     */
+    minKnightMoves(x, y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        const queue = [[0, 0]];
+        
+        const visited = new Set();
+        visited.add(key(0, 0));
+
+        let steps = 0;
+        // let closestDistSquared = Number.MAX_SAFE_INTEGER;
+
+        while (true) {
+            const queueLength = queue.length;
+            for (let i = 0; i < queueLength; i++) {
+                const [xCurr, yCurr] = queue.shift();
+
+                if (xCurr === x && yCurr === y) {
+                    return steps;
+                }
+
+                // const targetDistanceSquared = (x - xCurr) ** 2 + (y - yCurr) ** 2;
+                // if (targetDistanceSquared > closestDistSquared + 2 ** 2) {
+                //     continue;
+                // }
+
+                // if (visited.has(key(xCurr, yCurr))) {
+                //     continue;
+                // }
+
+                // closestDistSquared = Math.min(closestDistSquared, targetDistanceSquared)
+
+                // console.log(queue)
+
+                if (xCurr < -2 || yCurr < -2) {
+                    continue;
+                }
+
+                for (const [xDir, yDir] of MOVES) {
+                    const xNew = xCurr + xDir;
+                    const yNew = yCurr + yDir;
+                    if (visited.has(key(xNew, yNew)))
+                        continue;
+                    visited.add(key(xNew, yNew));
+                    queue.push([xNew, yNew]);
+                }
+            }
+
+            steps++;
+        }
+
+    }
+}
