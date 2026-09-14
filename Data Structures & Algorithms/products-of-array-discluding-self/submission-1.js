1627ac839faf4cf646ebd2293963f97be064ff6e
b1ankk <b1ankk-dev@pm.me>
2026-09-14 21:09:17 +0200

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+
+        const result = [];
+        result[0] = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1]
+        }
+
+        let postfix = 1;
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] = result[i] * postfix;
+            postfix *= nums[i];
+        }
+
+        return result;
+    }
+}
