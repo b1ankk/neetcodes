bbb7752332f30d455c2d84b2d3c55f77083cd922
b1ankk <b1ankk-dev@pm.me>
2026-09-21 23:43:53 +0200

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = '';
+
+        for (let i = 0; i < strs.length; i++) {
+            const string = strs[i];
+            for (const char of string) {
+                if (char === ';') {
+                    encoded += '\\;';
+                } else if (char === '\\') {
+                    encoded += '\\\\';
+                } else {
+                    encoded += char;
+                }
+
+
+            }
+
+            // if (i != strs.length - 1)
+            encoded += ';';
+        }
+
+        console.log(encoded)
+
+        return encoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if (str === '') {
+            return [];
+        }
+
+        return str.split(/(?<!\\);|(?<=\\\\);/).map(string => string.replaceAll('\\;', ';').replaceAll('\\\\', '\\')).slice(0, -1);
+
+    }
+}
