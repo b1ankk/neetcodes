2aa1d7900c88a27d673c24883b875abc940e0e2d
b1ankk <b1ankk-dev@pm.me>
2026-09-26 18:03:29 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (right >= left) {
+            const mid = Math.floor((right + left) / 2);
+            const midVal = nums[mid];
+
+            if (target < midVal) {
+                right = mid - 1;
+            } else if (target > midVal) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
